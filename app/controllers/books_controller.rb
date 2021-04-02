66336c7e2dcd7f605971485bfea7feda6dfd70d1
RIKO-tlov <bluebyrair@outlook.jp>
2021-04-02 05:41:56 +0000

    [continue]assignment2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,6 +31,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
+    flash[:notice] = "Book was successfully updated."
   end
 
   def destroy
