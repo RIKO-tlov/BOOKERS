8d86a7d27d1666bcda4733929b9e0b814f1f0402
RIKO-tlov <bluebyrair@outlook.jp>
2021-04-03 12:09:27 +0000

    [finish]assignment2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,17 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-  end
-
-  def new
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id)
-      flash[:notice] = "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
+       flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
     end
   end
 
@@ -28,10 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
-    flash[:notice] = "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -46,7 +49,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
